3178423f2f0624c277046cf6c3c88c2fd2d9a8f1
Frank Rendo <tmgestion@gmail.com>
2024-05-20 17:02:38 -0300

    Validar Contact

---
+++ js/validarContact.js
@@ -0,0 +1,38 @@
+// const formulario = document.getElementById('formulario');
+const emailInput = document.getElementById('email');
+const nameInput = document.getElementById('name');
+// const phoneInput = document.getElementById('phone');
+
+formulario.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const name = nameInput.value.trim();
+    const email = emailInput.value.trim();
+    if (validateName(name)) {
+        // alert('Nombre válido');
+        if (validateEmail(email)) {
+                alert('Nombre y Correo electrónico Okay');
+            } else {
+                alert('Email no válido');
+            }
+    } else {
+        alert('Nombre no válido');
+    }
+});
+
+function validateEmail(email) {
+    const regexemail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!regexemail.test(email)) {
+        return false;
+    } else{
+        return true;
+    }
+}
+
+function validateName(name) {
+     const regex2 = /^[a-zA-ZÀÁÇÈÉÍÏÑÓÖÚÜñáçèéíïñóöúü\s'-]+$/;
+    if (!regex2.test(name)) {
+        return false;
+    }
+        return true;
+}
